2c519f2cceb076913a0fb01d706a05e0c680abac
PHẠM NGỌC THANH <24BM0201043@tmu.edu.vn>
2025-06-13 16:22:26 +0700

    Update serializers.py

--- speech/serializers.py
+++ speech/serializers.py
@@ -6,5 +6,6 @@ from .models import AudioFile
 class AudioFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = AudioFile
-        fields = ["id", "audio", "transcript", "dialogues"]
-        read_only_fields = ["transcript", "dialogues"]
+        fields = ["id", "audio", "language", "status", "transcript", "dialogues"]
+        read_only_fields = ["transcript", "dialogues", "status"]
+
