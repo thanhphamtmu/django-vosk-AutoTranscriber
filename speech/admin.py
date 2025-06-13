e262ee3fc7fe2665c28d25e82e6beece88c99654
PHẠM NGỌC THANH <24BM0201043@tmu.edu.vn>
2025-06-13 10:40:11 +0700

    Update admin.py

--- speech/admin.py
+++ speech/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-
 from .models import AudioFile
-
 
 @admin.register(AudioFile)
 class AudioFileAdmin(admin.ModelAdmin):
-    list_display = ("id", "audio", "short_transcript")
+    list_display = ("id", "audio", "language", "status", "short_transcript")
     readonly_fields = ("transcript", "dialogues")
 
     def short_transcript(self, obj):
